b99f4c306c5f554da16e592283384b05fbe86ef5
kirp@umich.edu <tic-top>
2024-09-26 23:46:53 +0000

    cache draft for eager attention

---
+++ test.sh
@@ -0,0 +1,6 @@
+ruff format tests src utils
+pytest tests/models/kosmos2_5/test_modeling_kosmos2_5.py::Kosmos2_5ModelTest::test_batching_equivalence
+RUN_SLOW=True tests/models/kosmos2_5/test_modeling_kosmos2_5.py::Kosmos2_5ModelTest::test_batching_equivalence
+RUN_SLOW pytest tests/models/kosmos2_5/test_modeling_kosmos2_5.py::Kosmos2_5ModelTest::test_batching_equivalence
+RUN_SLOW=yes tests/models/kosmos2_5/test_modeling_kosmos2_5.py::Kosmos2_5ModelTest::test_batching_equivalence
+RUN_SLOW=yes pytest tests/models/kosmos2_5/test_modeling_kosmos2_5.py::Kosmos2_5ModelTest::test_batching_equivalence
